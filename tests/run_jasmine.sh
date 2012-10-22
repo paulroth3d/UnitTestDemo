e81ed73f5a3a6e3bc7e7af865e5c4ca85100c289
Paul Roth <paulroth3d@gmail.com>
2012-10-22 09:32:49 -0500

    UPDATE: add in a condition on whether to exit or simply echo out the results.

--- tests/run_jasmine.sh
+++ tests/run_jasmine.sh
@@ -1,9 +1,17 @@
 clear
 
+#exitOnCompletion="true";
+exitOnCompletion="false";
+
 ### Check for errors; abandon the process if error exist
 function checkError {
     if [[ $1 != 0 ]] ; then
-        exit 99
+        if [ $exitOnCompletion == "true" ]; then {
+			echo "there were failures. now returning"
+			#exit 99
+		}; else {
+			echo "There were failures";
+		}; fi;
     fi
 }
 
@@ -11,5 +19,10 @@ echo "*** Jasmine Tests... ***"
 phantomjs Jasmine-Runner.js
 checkError $?
 
-### Exit successfully
-exit 0
+
+if [ $exitOnCompletion == "true" ]; then {
+	echo "successful. now returning."
+	#exit 0
+}; else {
+	echo "successful"
+}; fi;
